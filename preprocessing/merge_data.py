e6c3c0582161806388772414196a7f8f54f68998
Anna Buch <a.buch@stud.uni-heidelberg.de>
2023-04-18 13:24:27 +0200

    repair dimensions of fact and masks

--- preprocessing/merge_data.py
+++ preprocessing/merge_data.py
@@ -2,30 +2,28 @@ import os
 import glob
 import subprocess
 from pathlib import Path
-
+import xarray as xr
+import settings as s
 
 
 variable_list = ["tas"]
-#variable_list = ["tx", "tn", "apdt", "tas", "tasrange", "tasskew"]  # tx, tn=tasmax, tasmin
 #variable_list = ["hurs", "tas", "pr6", "rg", "tasrange", "tasskew", "ws"]
 #variable_list = ["tas", "tasmax", "tasmin", "pr6", "rg", "ps", "sfcwind", "rsds", "rlds", "hurs", "huss"]
-dataset = "ERA5"
 timespane_list = ["1950_1989", "1990_2020"] # 
 time_hour = "12"
-tile_list = ["00024"]
+tile_list = ["00023"]
 
 source_base = Path(
     #"/p/projects/isimip/isimip/data/obsclim_harmonization/data_out_pp_combined_backup_for_attrici_paper/"
     #"/p/tmp/dominikp/meteo_data/BASD_c_attrici_test/"
     "/p/projects/ou/rd3/dmcci/basd_era5-land_to_efas-meteo/basd_output_data/"
-    #"/p/tmp/annabu/attrici_interpolation/meteo_data/ERA5/testarea_3001/"
 )
 
 source_dir = source_base 
 
 output_base = Path("/p/tmp/annabu/attrici_interpolation/meteo_data/")
 
-output_dir = output_base / dataset 
+output_dir = output_base / s.dataset 
 output_dir.mkdir(parents=True, exist_ok=True)
 
 
@@ -34,35 +32,20 @@ for variable in variable_list:
     for tile in tile_list:
     
         for timespane in timespane_list:
-     
-            output_file_ncpdq = output_dir / Path(variable + time_hour + "_" + dataset.lower() + "_" + timespane + "_" + tile + "_ba_ncpdq.nc")
+        
+            meteo_file = str(source_dir) + "/" + f"{s.dataset}_{variable}{time_hour}_{s.dataset}_{timespane}_t_{tile}_ba.nc"
+            output_file_ncpdq = output_dir / Path(variable + time_hour + "_" + s.dataset.lower() + "_" + timespane + "_" + tile + "_ba_n.nc4")
             timeunit_year = timespane.split('_')[0]
-     
-            print(f"\npermuate dimensions to time-lon-lat and time unit to daily:") 
-            # permutate dimensions to obtain a lonlat grid (lon,lat=grid) from a generic grid (where time,lat=grid)
+
+            ## set all facts to same dimension order
             cmd = (
-                "module load nco && ncpdq -4 -a time,lon,lat "  # -4 = as NECDF4
-                + str(source_dir)
-                + "/"
-                + f"{dataset}"
-                + "_"
-                + variable
-                + time_hour   #  12 oclock, 
-                + "_"
-                + dataset 
-                + "_"
-                + timespane
-                + "_t_"   # last tile of 25 tiles per timespane, 
-                + tile
-                + "_ba.nc"  # ???? == 4 unknown letter e.g. year
-                + " "
-                + str(output_file_ncpdq)
+                "module load nco && ncpdq -4 -a time,lon,lat " + meteo_file + " " + str(output_file_ncpdq)
             )
             print(cmd)
             subprocess.check_call(cmd, shell=True)
-            
-            ## fix time unit from hour to daily
-            output_file_setreftime = output_dir / Path(variable + time_hour + "_" + dataset.lower() + "_" + timespane + "_" + tile + "_ba_setreftime.nc")
+
+            ## repair time unit from hour to daily
+            output_file_setreftime = output_dir / Path(variable + time_hour + "_" + s.dataset.lower() + "_" + timespane + "_" + tile + "_ba_n_srt.nc4")
             cmd = (
                 f"module load cdo && cdo -setreftime,{timeunit_year}-01-01,{time_hour}:00:00,1day "
                 + str(output_file_ncpdq)
@@ -71,25 +54,40 @@ for variable in variable_list:
             )
             print(cmd)
             subprocess.check_call(cmd, shell=True)
-     
-        
+
+            ## Interpolation test subset: crop to spatial subset to nth x nth cells
+            meteo = xr.open_dataset(output_file_setreftime)
+            meteo_clipped_file = str(output_dir) + "/" + variable + time_hour + "_" + s.dataset.lower() + "_" + timespane + "_" + tile + "_ba_n_srt_c.nc4"
+            print(meteo) 
+            meteo_clipped = meteo.isel(lon=slice(0,s.file_len), lat=slice(0, s.file_len))  # select by index
+            print(meteo_clipped)
+            meteo_clipped.to_netcdf(meteo_clipped_file)
+            meteo_clipped.close()
+            print("clipping by spatial index - done")
+
+
         # merge separated nc files by variable to one merged nc file
-        output_file_mergetime = output_dir / Path(variable + time_hour + "_" + dataset.lower() + "_" + "1950_2020" + "_" + tile +"_ba_ncpdq_merged.nc")
+        output_file_mergetime = output_dir / Path(variable + time_hour + "_" + s.dataset.lower() + "_" + "1950_2020" + "_" + tile +"_ba_n_srt_c_merged.nc4")
         print(f"\nwriting to {output_file_mergetime}:") 
         cmd = (
             "module load cdo && cdo mergetime " 
-            + str(output_dir) + "/" + variable + time_hour + "_" + dataset.lower() + "_????_????_" + tile + "_ba_setreftime.nc4"  # infiles from output_file_setreftime
+            + str(output_dir) + "/" + variable + time_hour + "_" + s.dataset.lower() 
+            + "_????_????_"
+            + tile + "_ba_n_srt_c.nc4"
             + " "
             + str(output_file_mergetime)
         )
         print(cmd)
         subprocess.check_call(cmd, shell=True)
 
+        ## reorder dimensions of fact file
+        meteo = xr.open_dataset(output_file_mergetime)
+        meteo_dim_file = str(output_dir) + "/" + variable + time_hour + "_" + s.dataset.lower() + "_" + "1950_2020" + "_" + tile + "_ba_n_srt_c_merged_d.nc4"
+        print(meteo) 
+        meteo_dim = meteo
+        meteo_dim["tas"] = meteo_dim["tas"].transpose("time", "lat", "lon")
+        print(meteo_dim)
+        meteo_dim.to_netcdf(meteo_dim_file, format="NETCDF4")
+        meteo_dim.close()
+        print("clipping by spatial index - done")
 
-
-# remove intermediate files
-for r in glob.glob([str(output_dir)+"/*_ba_setreftime.nc", str(output_dir)+"/*_ba_ncpdq.nc"], recursive=True):
-    try:
-        os.remove(r)
-    except e:
-        print("Couldnt find file: ", r)
