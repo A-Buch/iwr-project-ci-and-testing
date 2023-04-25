3d630f7fa92414bd1a8db5cac9fb0195a0f1e677
Simon Treu <sitreu@pik-potsdam.de>
2023-04-25 13:52:16 +0200

    Changed interpolate_parameters to use xarray
    Also added a jupyter notebook to
    * merge parameters into one netcdf file
    * write parameters from single merged netcdf file back to single pickle
      files, one per grid cell. After that they have the same form as output
    by pymc
    * test that merging parameters and writing them back to single files
      does not change anything
    
    The first function is already implemented in interpolate_parameters.py
    in this commit. The second function still needs to be included into the
    main code. The third function may be implemented as unittest. When all
    functions are implemented the notebook may be deleted

--- interpolate_parameters.py
+++ interpolate_parameters.py
@@ -1,200 +1,150 @@
-import os
-import numpy as np
-import netCDF4 as nc
-import xarray as xr
-import matplotlib.pylab as plt
+"""Functions to merge single parameter files into a global netcdf file,
+reduce the number of parameters and interpolate missing parameters"""
+import pickle
+import re
 import subprocess
 from pathlib import Path
+
+import numpy as np
+import xarray as xr
 from scipy.ndimage import minimum_filter
-import pickle
-import itertools as it
 
 import settings as s
-import attrici.datahandler as dh
-import attrici.postprocess as pp
 
 
-# remove_intermediate_files = False
-
-# def interpolation_parameters(parameter_filepath, landsea_mask_filepath):
-    # """
-    # params: 
-    # return: filepath to interpolated parameter file
-    # """
-
-## create parameter file from trace files
-
-## create file to store parameters if not exists, otherwise interpolate existing file
-input_file = s.input_dir / s.dataset / s.testarea / s.source_file.lower()
-landsea_mask_filepath = s.input_dir /  s.dataset / s.testarea / s.landsea_file
-
-obs_data = nc.Dataset(input_file, "r")
-nct = obs_data.variables["time"]
-lats = obs_data.variables["lat"][:]
-lons = obs_data.variables["lon"][:]
-
-# parameter_filepath = s.output_dir / s.trace_file
-# if not os.path.exists(parameter_filepath):
-    # out = nc.Dataset(parameter_filepath, "w", format="NETCDF4") 
-    # pp.form_global_nc(out, nct[:8], lats, lons, None, nct.units)
-    # out.close()
-
-# parameter_file = nc.Dataset(parameter_filepath, "a", format="NETCDF4") 
-# parameter_file = xr.open_dataset(parameter_filepath) 
+def get_float_from_string(file_name):
+    """Returns a float if there is exactly one float in the string.
+    Otherwise it throws a ValueError"""
+    floats_in_string = re.findall(r"[-+]?(?:\d*\.*\d+)", file_name)
+    if len(floats_in_string) != 1:
+        raise ValueError("there is no ore more than one float in this string")
+    return float(floats_in_string[0])
 
 
-### check which data is available
-data_list = []
-lat_indices = []
-lon_indices = []
-lat_float_l = []
-lon_float_l = []
+def merge_parameters(trace_dir, parameter_filepath):
+    """Merge all files in trace_dir into a single netCDF4 file
+    which is stored in parameter_filepath"""
+    parameter_files = []
+    for trace_file in trace_dir.glob("**/lon*"):
+        lat = get_float_from_string(trace_file.parent.name)
+        lon = get_float_from_string(trace_file.name)
+        data_vars = []
+        with open(trace_file, "rb") as trace:
+            free_params = pickle.load(trace)
+        for key in free_params.keys():
+            try:
+                dim_param = np.arange(len(free_params[key]))
+            except TypeError as error:
+                if str(error) == "len() of unsized object":
+                    dim_param = np.arange(1)
+                else:
+                    raise error
 
-trace_dir = s.output_dir / "traces" / s.variable
-for i in trace_dir.glob("**/lon*"):
-    data_list.append(str(i))
-    lat_float = float(str(i).split("lat")[-1].split("_")[-1].split("/")[0])
-    lon_float = float(str(i).split("lon")[-1])
-    lat_float_l.append(lat_float)
-    lon_float_l.append(lon_float)
+            data_vars.append(
+                xr.DataArray(
+                    dims=["d", "lat", "lon"],
+                    data=free_params[key].reshape((-1, 1, 1)),
+                    coords={
+                        "d": ("d", dim_param),
+                        "lat": ("lat", [lat]),
+                        "lon": ("lon", [lon]),
+                    },
+                    name=key,
+                )
+            )
+        parameter_files.append(xr.merge(data_vars))
 
-## get each coordinate index for regional AOI
-## TODO: test with other lateral_sub
-if s.lateral_sub == 1 :
-    lat_indices.append( pp.rescale_aoi(lat_float_l))
-    lon_indices.append( pp.rescale_aoi(lon_float_l))
-
-# TODO: make this nicer
-## remove double list
-[lat_indices ] = lat_indices
-[lon_indices ] = lon_indices
-
-
-for (i, j, dfpath) in it.zip_longest(lat_indices, lon_indices, data_list):
-#for outdir_for_cell in data_list:
-#    try:
-    print("Writing parameters  from trace files to parameter file.")
-    with open(dfpath, 'rb') as handle:
-        free_params = pickle.load(handle) ## load from trace.h5 files
-
-    # write the values of each parameter as single layers to nc file, i.e. one parameter can contain 1 to n layers
-    param_names = list(free_params.keys())
-
-    parameter_filepath = s.output_dir / s.trace_file
-    if not os.path.exists(parameter_filepath):
-        out = nc.Dataset(parameter_filepath, "w", format="NETCDF4") 
-        pp.form_global_nc(out, nct[:8], lats, lons, param_names, nct.units)
-        out.close()
-    parameter_file = nc.Dataset(parameter_filepath, "a", format="NETCDF4") 
-    #parameter_file = xr.open_dataset(parameter_filepath) 
-    print(parameter_file)
-
-    for param_name in param_names:
-        values_per_parameter = np.atleast_1d(np.array(free_params[param_name])) # forcing 0-D arrays to 1-D
-        # ## create empty variable in netcdf if not exists
-        if param_name in parameter_file.variables.keys():
-            pass
-        else:
-            #parameter_file.variables[param_name][ :, int(i), int(j)] = (('time', 'lat', 'lon'), values_per_parameter) 
-            # parameter_file.variables[param_name][ :, int(i), int(j)] = xr.Dataset({'parameter_file': parameter_file.variables[param_name][ :, int(i), int(j)], f'{param_name}': (('time', 'lat', 'lon'), values_per_parameter)})
-            parameter_file.createVariable(param_name, "f4", ("time", "lat", "lon"), chunksizes=(nct[:8].shape[0], 1, 1), fill_value=1e20) 
-        parameter_file.variables[param_name][ :, int(i), int(j)] = values_per_parameter  ## file opened with NETCDF4 package
-        # for n in range(len(values_per_parameter)):  ## file opened with xarray
-        #    parameter_file.variables[param_name][ n, int(i), int(j)] = values_per_parameter[n] #np.array(values_per_parameter[n])
-           #     parameter_file.variables[param_name][ n, int(i), int(j)] = parameter_file.assign(f"{param_name}"= values_per_parameter[n])
-    print(f"wrote all {len(param_names)} to cell position",  int(i), int(j))
-print(parameter_file)
-
-parameter_file.close()
+    merged_parameters = xr.merge(parameter_files)
+    merged_parameters.to_netcdf(parameter_filepath, format="NETCDF4")
+    return merge_parameters
 
 
+def reduce_parameters(parameter_file, bmask, parameter_filepath_m, support_filepath):
+    """
+    Use a (binary) mask file to reduce the number of parameters. This function is only for testing.
+    In production we want to only compute parameters for the support cells
+    (defined in the mask file)
+    and interpolate the parameters in between using interpolate_parameters().
+    This function should be used if the effects of interpolation are evaluated
+    against computing parameters for all files
+    """
+    parameter_file_m = parameter_file * bmask.binary_mask[0, :, :]
 
-## load binary mask of nth * nth cells
-bmask_filepath = s.input_dir / s.dataset / s.testarea / s.bmask_file
-bmask = xr.open_dataset(bmask_filepath)
-print("binary mask:\n", bmask)
+    coastmask = parameter_file.notnull()
+    coastmask = minimum_filter(
+        coastmask.weights_fc_trend, size=(0, 3, 3), mode="nearest"
+    )
+    coastlines = parameter_file.where(~coastmask)  # extract coast lines
+    parameter_file_m = parameter_file_m.merge(
+        coastlines, join="outer", compat="no_conflicts"
+    )  ###  every 3rd cell + cells from coastline are support cells
 
-## get support cells from binary mask and coastline
-parameter_file = xr.open_dataset(parameter_filepath)
-parameter_file_m = parameter_file * bmask.binary_mask[0,:,:] # every 3rd cell is support cell
-    
-coastmask = parameter_file.notnull()
-#m = minimum_filter(mask, size=2, mode='reflect')
-coastmask = minimum_filter(coastmask.weights_fc_trend, size=(0,3,3), mode='nearest')
-coastlines = parameter_file.where(~coastmask) # extract coast lines
-parameter_file_m = parameter_file_m.merge(coastlines, join="outer", compat="no_conflicts") ###  every 3rd cell + cells from coastline are support cells
+    parameter_file_m.to_netcdf(parameter_filepath_m, format="NETCDF4")
+    parameter_file_m.close()
 
-parameter_filepath_m = Path.joinpath(s.output_dir, Path(parameter_filepath).stem + "_m.nc4")
-parameter_file_m.to_netcdf(parameter_filepath_m, format="NETCDF4")
-parameter_file_m.close()
+    ##  let cdo recognize nan values in masked parameters file
+    print("set masked values of parameter file as nan")
+    cmd = (
+        "cdo setmissval,nan " + str(parameter_filepath_m) + " " + str(support_filepath)
+    )
 
-##  let cdo recognize nan values in masked parameters file
-print("set masked values of parameter file as nan")
-parameter_filepath_m_nan = Path.joinpath(s.output_dir, Path(parameter_filepath_m).stem + "_nan.nc4")
-
-cmd = "cdo setmissval,nan " + str(parameter_filepath_m) + " " + str(parameter_filepath_m_nan)
-#cmd = "cdo -setmissval,nan " + str(parameter_filepath_nan_m) + " " + str(parameter_filepath_nan_m_nan)
-
-try:
-    print(cmd)
-    subprocess.check_call(cmd, shell=True)
-except subprocess.CalledProcessError:
-    cmd = "module load cdo && " + cmd
-    print(cmd)
-    subprocess.check_call(cmd, shell=True)
-
-
-## converting Normal value to NaN value so that cdo interprets them as missing values during interpolation
-#cmd = "cdo setmissval,nan " + masked_params_filepath + f" {outpath}/tas_trace_shape16_masked_setmissval_nan.nc4"
-
-## interpolate missing values in paramters file
-print("interpolating..")
-## [setmisstodis, neighbors]: distance-weighted average of the nearest non missing values
-parameter_filepath_m_nan_n4 = Path.joinpath(s.output_dir, Path(parameter_filepath_m_nan).stem + "_n4.nc4")
-
-cmd = "cdo -setmisstodis,4 " +  str(parameter_filepath_m_nan) + " " + str(parameter_filepath_m_nan_n4) 
-
-try:
-    print(cmd)
-    subprocess.check_call(cmd, shell=True)
-except subprocess.CalledProcessError:
-    cmd = "module load cdo && " + cmd
-    print(cmd)
-    subprocess.check_call(cmd, shell=True)
-
-## clip new interpolated parameter file with landsea_mask to remove interpolation from sea area
-#input_dir + "/" + dataset + "/" + testarea  + f'/landseamask_{file_len}_setmissval.nc'
-landsea_mask = xr.open_dataset(landsea_mask_filepath)
-parameter_m_nan_n4 = xr.open_dataset(parameter_filepath_m_nan_n4)
-parameter_filepath_m_nan_n4_clipped = Path.joinpath(s.output_dir, Path(parameter_filepath_m_nan_n4).stem + "_c.nc4")
-
-## TODO: workaround to fix representation in python while dimensions of parameter file are the same as in landsea_mask
-#parameter_m_nan_n4_clipped = parameter_m_nan_n4 * np.array(landsea_mask["mask"][0, :, :]).reindex(lat=landsea_mask.lat[::-1])) # landseamask.lat is flipped in relation to parameter file
-parameter_m_nan_n4_clipped = parameter_m_nan_n4 * landsea_mask["mask"][0, :, :]
-
-parameter_m_nan_n4_clipped.to_netcdf(parameter_filepath_m_nan_n4_clipped, format="NETCDF4")
-print(f"Generated interpolated parameters, stored in {parameter_filepath_m_nan_n4_clipped}")
-parameter_m_nan_n4_clipped.close()
-
-# cmd = "cdo -mul " + str(landsea_mask_filepath) + " " + str(parameter_filepath_m_nan_n4) + " " + str(parameter_filepath_m_nan_n4_clipped) 
-# try:
-    # print(cmd)
-    # subprocess.check_call(cmd, shell=True)
-# except subprocess.CalledProcessError:
-    # cmd = "module load cdo && " + cmd
-    # print(cmd)
-    # subprocess.check_call(cmd, shell=True)
+    try:
+        print(cmd)
+        subprocess.check_call(cmd, shell=True)
+    except subprocess.CalledProcessError:
+        cmd = "module load cdo && " + cmd
+        print(cmd)
+        subprocess.check_call(cmd, shell=True)
 
 
-# if remove_intermediate_files == True:
-    # print(f"Removing: {parameter_filepath_nan_m, parameter_filepath_nan_m_nan}")
-    # os.remove(parameter_filepath_nan)
-    # os.remove(parameter_filepath_nan_m)
-    # os.remove(parameter_filepath_nan_m_nan)
-    
-# return parameter_m_nan_n4_clipped
+def interpolate_parameters(support_filepath, interpolated_parameter_filepath):
+    """interpolate missing values in paramters file"""
+    print("interpolating..")
+    ## [setmisstodis, neighbors]: distance-weighted average of the nearest non missing values
+    cmd = (
+        "cdo -setmisstodis,4 "
+        + str(support_filepath)
+        + " "
+        + str(interpolated_parameter_filepath)
+    )
+
+    try:
+        print(cmd)
+        subprocess.check_call(cmd, shell=True)
+    except subprocess.CalledProcessError:
+        cmd = "module load cdo && " + cmd
+        print(cmd)
+        subprocess.check_call(cmd, shell=True)
+
+    print(
+        f"Generated interpolated parameters, stored in {interpolated_parameter_filepath}"
+    )
 
 
+def main():
+    """All steps from single parameter files for each grid cell to a interpolated parameter file"""
+    parameter_file = merge_parameters(
+        trace_dir=s.output_dir / "traces" / s.variable,
+        parameter_filepath=s.output_dir / s.trace_file,
+    )
+
+    ## load binary mask
+    bmask_filepath = s.input_dir / s.dataset / s.testarea / s.bmask_file
+    bmask = xr.load_dataset(bmask_filepath)
+    print("binary mask:\n", bmask)
+
+    parameter_filepath_m = Path.joinpath(s.output_dir, s.trace_file.stem + "_m.nc4")
+    support_filepath = Path.joinpath(
+        s.output_dir, Path(parameter_filepath_m).stem + "_nan.nc4"
+    )
+
+    reduce_parameters(parameter_file, bmask, parameter_filepath_m, support_filepath)
+
+    interpolated_parameter_filepath = Path.joinpath(
+        s.output_dir, Path(support_filepath).stem + "_n4.nc4"
+    )
+    interpolate_parameters(support_filepath, interpolated_parameter_filepath)
 
 
-
+if __name__ == "__main__":
+    main()
