0cac39549ac796c0e82710423de62c539cd1012c
Anna Buch <a.buch@stud.uni-heidelberg.de>
2023-04-27 14:49:13 +0200

    del intermediate files

--- preprocessing/merge_data.py
+++ preprocessing/merge_data.py
@@ -6,6 +6,8 @@ import xarray as xr
 import settings as s
 
 
+remove_intermediate_files = True
+
 variable_list = ["tas"]
 #variable_list = ["hurs", "tas", "pr6", "rg", "tasrange", "tasskew", "ws"]
 #variable_list = ["tas", "tasmax", "tasmin", "pr6", "rg", "ps", "sfcwind", "rsds", "rlds", "hurs", "huss"]
@@ -14,7 +16,6 @@ time_hour = "12"
 tile_list = ["00023"]
 
 source_base = Path(
-    #"/p/projects/isimip/isimip/data/obsclim_harmonization/data_out_pp_combined_backup_for_attrici_paper/"
     #"/p/tmp/dominikp/meteo_data/BASD_c_attrici_test/"
     "/p/projects/ou/rd3/dmcci/basd_era5-land_to_efas-meteo/basd_output_data/"
 )
@@ -34,7 +35,7 @@ for variable in variable_list:
         for timespane in timespane_list:
         
             meteo_file = str(source_dir) + "/" + f"{s.dataset}_{variable}{time_hour}_{s.dataset}_{timespane}_t_{tile}_ba.nc"
-            output_file_ncpdq = output_dir / Path(variable + time_hour + "_" + s.dataset.lower() + "_" + timespane + "_" + tile + "_ba_n.nc4")
+            output_file_ncpdq = output_dir / Path(variable + time_hour + "_" + s.dataset.lower() + "_" + timespane + "_" + tile + "_ba_n_tmp.nc4")
             timeunit_year = timespane.split('_')[0]
 
             ## set all facts to same dimension order
@@ -45,7 +46,7 @@ for variable in variable_list:
             subprocess.check_call(cmd, shell=True)
 
             ## repair time unit from hour to daily
-            output_file_setreftime = output_dir / Path(variable + time_hour + "_" + s.dataset.lower() + "_" + timespane + "_" + tile + "_ba_n_srt.nc4")
+            output_file_setreftime = output_dir / Path(variable + time_hour + "_" + s.dataset.lower() + "_" + timespane + "_" + tile + "_ba_n_srt_tmp.nc4")
             cmd = (
                 f"module load cdo && cdo -setreftime,{timeunit_year}-01-01,{time_hour}:00:00,1day "
                 + str(output_file_ncpdq)
@@ -56,8 +57,8 @@ for variable in variable_list:
             subprocess.check_call(cmd, shell=True)
 
             ## Interpolation test subset: crop to spatial subset to nth x nth cells
-            meteo = xr.open_dataset(output_file_setreftime)
-            meteo_clipped_file = str(output_dir) + "/" + variable + time_hour + "_" + s.dataset.lower() + "_" + timespane + "_" + tile + "_ba_n_srt_c.nc4"
+            meteo = xr.load_dataset(output_file_setreftime)
+            meteo_clipped_file = str(output_dir) + "/" + variable + time_hour + "_" + s.dataset.lower() + "_" + timespane + "_" + tile + "_ba_n_srt_c_tmp.nc4"
             print(meteo) 
             meteo_clipped = meteo.isel(lon=slice(0,s.file_len), lat=slice(0, s.file_len))  # select by index
             print(meteo_clipped)
@@ -67,7 +68,7 @@ for variable in variable_list:
 
 
         # merge separated nc files by variable to one merged nc file
-        output_file_mergetime = output_dir / Path(variable + time_hour + "_" + s.dataset.lower() + "_" + "1950_2020" + "_" + tile +"_ba_n_srt_c_merged.nc4")
+        output_file_mergetime = output_dir / Path(variable + time_hour + "_" + s.dataset.lower() + "_" + "1950_2020" + "_" + tile +"_ba_n_srt_c_merged_tmp.nc4")
         print(f"\nwriting to {output_file_mergetime}:") 
         cmd = (
             "module load cdo && cdo mergetime " 
@@ -81,8 +82,8 @@ for variable in variable_list:
         subprocess.check_call(cmd, shell=True)
 
         ## reorder dimensions of fact file
-        meteo = xr.open_dataset(output_file_mergetime)
-        meteo_dim_file = str(output_dir) + "/" + variable + time_hour + "_" + s.dataset.lower() + "_" + "1950_2020" + "_" + tile + "_ba_n_srt_c_merged_d.nc4"
+        meteo = xr.load_dataset(output_file_mergetime)
+        meteo_dim_file = str(output_dir) + "/" + variable + time_hour + "_" + s.dataset.lower() + "_" + "1950_2020" + "_" + tile + "_ba_merged.nc4"
         print(meteo) 
         meteo_dim = meteo
         meteo_dim["tas"] = meteo_dim["tas"].transpose("time", "lat", "lon")
@@ -91,3 +92,8 @@ for variable in variable_list:
         meteo_dim.close()
         print("clipping by spatial index - done")
 
+
+if remove_intermediate_files == True:
+    for tmp_files in glob.iglob(os.path.join(dir, '*_tmp.*')):
+        os.remove(tmp_files)
+    print(f"Removing temporary files")
