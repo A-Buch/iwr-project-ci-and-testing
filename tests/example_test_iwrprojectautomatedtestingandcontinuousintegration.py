2ab8f0d9da70e50b586796eaa1c4e9cfdc796103
A-Buch <a.buch@stud.uni-heidelberg.de>
2024-01-02 10:01:19 +0100

    fix and update ci

--- tests/example_test_iwrprojectautomatedtestingandcontinuousintegration.py
+++
@@ -1,5 +0,0 @@
-import iwrprojectautomatedtestingandcontinuousintegration
-
-
-def test_iwrprojectautomatedtestingandcontinuousintegration():
-    assert iwrprojectautomatedtestingandcontinuousintegration.add_one(1) == 2
